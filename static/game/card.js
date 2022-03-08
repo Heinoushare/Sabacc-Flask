c5de83d579a62505058bacb0b161c8d92d0e62d0
Heinoushare <samuel.sudbury@gmail.com>
2022-03-08 23:23:55 +0000

    Tue, Mar 8, 2022, 11:23 PM +00:00

---
+++ static/game/card.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+    // Define Sockets
+	var socket = io.connect('https://heinoushare-code50-76819177-g4x99w676fvqvg-5000.githubpreview.dev');
+	var game_socket = io('https://heinoushare-code50-76819177-g4x99w676fvqvg-5000.githubpreview.dev/card');
+
+	
+
+});
