09379b02f66b5a7848ef6cb1a6929e91991db9ee
Heinoushare <samuel.sudbury@gmail.com>
2022-03-10 20:30:11 +0000

    Thu, Mar 10, 2022, 8:30 PM +00:00

--- static/game/game.js
+++ static/game/game.js
@@ -7,19 +7,4 @@ $(document).ready(function() {
 		game_socket.emit("game");
 	});
 
-	// Reduce website hackability by defining variables pulled from the HTML as soon as possible
-	var game = document.getElementById("game");
-	var player = document.getElementById("player");
-	var opponent = document.getElementById("opponent");
-
-	// Create variables off of attributes of "meta" variables
-	var completed = parseInt(game.getAttribute("completed"));
-	var winner = parseInt()
-
-	let playerID = parseInt(player.getAttribute("player_id"));
-	var username = plyaer.getAttribute("username");
-
-	let opponent_id = parseInt(opponent.getAttribute("opponent_id"));
-	let opponent_username = opponent.getAttribute("username");
-
 });
