ba33c68c088579d4d66a11ce9419413a7779c8fa
Heinoushare <76819177+Heinoushare@users.noreply.github.com>
2022-03-12 16:39:36 +0000

    Sat, Mar 12, 2022, 4:39 PM +00:00

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+# All library names must be in lowercase
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
--- helpers.py
+++ helpers.py
@@ -1,7 +1,7 @@
 from flask import redirect, render_template, request, session
 from functools import wraps
 import random
-from Flask_SocketIO import emit
+from flask_socketio import emit
 
 # Helper functions for application.py
 
