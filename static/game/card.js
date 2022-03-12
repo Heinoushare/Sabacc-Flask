dc6ea9434c044cf3b75108edf5a8345043d6769d
Heinoushare <76819177+Heinoushare@users.noreply.github.com>
2022-03-12 17:10:49 +0000

    Sat, Mar 12, 2022, 5:10 PM +00:00

--- static/game/card.js
+++ static/game/card.js
@@ -54,7 +54,7 @@ $(document).ready(function() {
 		else if (player_phrase === "player2")
 		{
 			hide("p1Card");
-			if ((player1_card === "None" || player1_card === "null") && player2_card != "None" && player2_card != "null")
+			if (player1_card != "None" && player1_card != "null" && player2_card === "None" && player2_card === "null")
 			{
 				hide("p2Trade");
 			}
