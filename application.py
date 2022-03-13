5aee0d31b7bf58c6e0963e241e8c87fff37d2a98
Heinoushare <76819177+Heinoushare@users.noreply.github.com>
2022-03-13 21:05:49 +0000

    Sun, Mar 13, 2022, 9:05 PM +00:00

--- application.py
+++ application.py
@@ -338,7 +338,7 @@ def card(data):
             p2Abs = abs(int(calcHandVal(list(game["player2_hand"].split(",")))))
             winner = winner(game)
 
-            db.execute(f"UPDATE games SET player2_card = ?, player_turn = ? WHERE game_id = {game_id}", player2_hand, action, -1)
+            db.execute(f"UPDATE games SET phase = ?, player2_card = ?, player_turn = ? WHERE game_id = {game_id}", player2_hand, action, -1)
             game = db.execute(f"SELECT * FROM games WHERE game_id = {game_id}")[0]
 
 
