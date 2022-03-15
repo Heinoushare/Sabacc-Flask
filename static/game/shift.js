6b23182296777782f83e7d6a8d77b39257317beb
Heinoushare <76819177+Heinoushare@users.noreply.github.com>
2022-03-15 15:12:21 +0000

    Tue, Mar 15, 2022, 3:12 PM +00:00

--- static/game/shift.js
+++ static/game/shift.js
@@ -76,8 +76,8 @@ $(document).ready(function() {
 		}
 
 		// Update HTML
-		document.getElementById("hand").innerHTML = data[player_phrase + "_hand"];
-		let opCards = data[opponent_phrase + "_hand"].split(",");
+		document.getElementById("procCards").innerHTML = data[player_phrase + "_protected"];
+		let opProc = data[opponent_phrase + "_hand"].split(",");
 		document.getElementById("opponent_cards").innerHTML = opCards.length;
 
 		return;
