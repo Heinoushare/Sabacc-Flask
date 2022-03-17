ffde8c7a69acb9dfbdf16866ebe39d46794b8f05
Heinoushare <76819177+Heinoushare@users.noreply.github.com>
2022-03-17 20:05:26 +0000

    Thu, Mar 17, 2022, 8:05 PM +00:00

--- static/game/shift.js
+++ static/game/shift.js
@@ -64,7 +64,7 @@ $(document).ready(function() {
 				break;
 			}
 		}
-		$("#shiftable").append('<input type="checkbox" id="' + card + '" name="' + pHand[card] + '" value="' + pHand[card] + '>' + phand[card] + '</input>');
+		$("#shiftable").append('<input type="checkbox" id="' + card + '" name="' + pHand[card] + '" value="' + pHand[card] + '>' + pHand[card] + '</input>');
 		document.getElementById(card).checked = check;
 	}
 
