eebb4ad58a7b96836ded1e46ea45727b36f6021e
Heinoushare <76819177+Heinoushare@users.noreply.github.com>
2022-03-18 11:48:50 +0000

    Fri, Mar 18, 2022, 11:48 AM +00:00

--- application.py
+++ application.py
@@ -406,7 +406,7 @@ def card(data):
                 p2BombOut = 0.1
             sabGain = 0
             if p1BombOut == 0.1 and p2BombOut == 0.1:
-                sabGain = game["hand_pot"]
+                sabGain = 0.2
 
             p1Gain = 0
             p2Gain = 0
@@ -431,7 +431,7 @@ def card(data):
 
             # TODO Sudden demise, rounding bomb out, winning and losing credits players and pots
 
-            db.execute(f"UPDATE games SET player1_credits = ?, player2_credits = ?, hand_pot = ?, sabacc_pot = ?, phase = ?, player2_card = ?, player_turn = ?, completed = ?, winner = ? WHERE game_id = {game_id}", game["player1_credits"] - round((game["hand_pot"] * p1BombOut)) + p1Gain, game["player2_credits"] - round((game["hand_pot"] * p2BombOut)) + p2Gain, game["hand_pot"] + round((game["player1_credits"] * (game["hand_pot"] * p1BombOut))) + round((game["player2_credits"] * (game["hand_pot"] * p2BombOut))) - handPotLoss, "completed", action, -1, 1, winner)
+            db.execute(f"UPDATE games SET player1_credits = ?, player2_credits = ?, hand_pot = ?, sabacc_pot = ?, phase = ?, player2_card = ?, player_turn = ?, completed = ?, winner = ? WHERE game_id = {game_id}", game["player1_credits"] - round((game["hand_pot"] * p1BombOut)) + p1Gain, game["player2_credits"] - round((game["hand_pot"] * p2BombOut)) + p2Gain, game["hand_pot"] + round((game["player1_credits"] * (game["hand_pot"] * p1BombOut))) + round((game["player2_credits"] * (game["hand_pot"] * p2BombOut))) - handPotLoss - sabGain, game["sabacc_pot"] + (game["hand_pot"] * sabGain), "completed", action, -1, 1, winner)
             game = db.execute(f"SELECT * FROM games WHERE game_id = {game_id}")[0]
 
         if game["player1_card"] == "alderaan":
