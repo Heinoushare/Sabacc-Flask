e196f1a255cd935cd415a66ca80cf99af3d3deb8
Heinoushare <76819177+Heinoushare@users.noreply.github.com>
2022-03-20 14:22:56 +0000

    Sun, Mar 20, 2022, 2:22 PM +00:00

--- static/index.js
+++ static/index.js
@@ -1,37 +1,22 @@
 $(document).ready(function() {
 
 
-	// $(".link").on("click", function() {
+	$(".link").on("click", function() {
 
-    //     /* Get the text field */
-    //     let link = $(this).attr("id");
-
-    //     /* Select the text field */
-    //     // link.select();
-    //     // link.setSelectionRange(0, 99999); /* For mobile devices */
-
-    //     /* Copy the text inside the text field */
-    //     navigator.clipboard.writeText(link);
-
-    //     /* Alert the copied text */
-    //     alert("Copied the text: " + link);
-    //     console.log(link);
-
-    // });
-    function copy(id)
-    {
+        /* Get the text field */
+        let link = $(this).attr("id");
 
         /* Select the text field */
         // link.select();
         // link.setSelectionRange(0, 99999); /* For mobile devices */
 
         /* Copy the text inside the text field */
-        let link = "https://heinoushare-code50-76819177-g4x99w676fvqvg-5000.githubpreview.dev/game/" + id;
         navigator.clipboard.writeText(link);
 
         /* Alert the copied text */
         alert("Copied the text: " + link);
         console.log(link);
-    }
+
+    });
 
 });
