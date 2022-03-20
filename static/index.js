df5b2c74c1d630b64bbaa5c8f126bf6d44e5fa3d
Heinoushare <76819177+Heinoushare@users.noreply.github.com>
2022-03-20 12:50:57 +0000

    Sun, Mar 20, 2022, 12:50 PM +00:00

--- static/index.js
+++ static/index.js
@@ -7,8 +7,8 @@ $(document).ready(function() {
         let link = $("this").attr("id"));
 
         /* Select the text field */
-        copyText.select();
-        copyText.setSelectionRange(0, 99999); /* For mobile devices */
+        link.select();
+        link.setSelectionRange(0, 99999); /* For mobile devices */
 
         /* Copy the text inside the text field */
         navigator.clipboard.writeText(copyText.value);
