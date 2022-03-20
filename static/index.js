79adb83dac8ff6a692d83f117198e5eab997789a
Heinoushare <76819177+Heinoushare@users.noreply.github.com>
2022-03-20 14:19:09 +0000

    Sun, Mar 20, 2022, 2:19 PM +00:00

--- static/index.js
+++ static/index.js
@@ -1,22 +1,37 @@
 $(document).ready(function() {
 
 
-	$(".link").on("click", function() {
+	// $(".link").on("click", function() {
 
-        /* Get the text field */
-        let link = $(this).attr("id");
+    //     /* Get the text field */
+    //     let link = $(this).attr("id");
+
+    //     /* Select the text field */
+    //     // link.select();
+    //     // link.setSelectionRange(0, 99999); /* For mobile devices */
+
+    //     /* Copy the text inside the text field */
+    //     navigator.clipboard.writeText(link);
+
+    //     /* Alert the copied text */
+    //     alert("Copied the text: " + link);
+    //     console.log(link);
+
+    // });
+    function copy(id)
+    {
 
         /* Select the text field */
         // link.select();
         // link.setSelectionRange(0, 99999); /* For mobile devices */
 
         /* Copy the text inside the text field */
+        link
         navigator.clipboard.writeText(link);
 
         /* Alert the copied text */
         alert("Copied the text: " + link);
         console.log(link);
-
-    });
+    }
 
 });
