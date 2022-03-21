7d4a5cd4cfb2e1ec753d833a44295af24a5659e3
Heinoushare <76819177+Heinoushare@users.noreply.github.com>
2022-03-21 01:26:33 +0000

    Mon, Mar 21, 2022, 1:26 AM +00:00

--- static/game/game.js
+++ static/game/game.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 
+	// Set socket variables
 	var socket = io.connect('https://heinoushare-code50-76819177-g4x99w676fvqvg-5000.githubpreview.dev');
 	var game_socket = io('https://heinoushare-code50-76819177-g4x99w676fvqvg-5000.githubpreview.dev/game');
 
+	// Open socket connection with server
 	game_socket.on('connect', function() {
 		game_socket.emit("game");
 	});
